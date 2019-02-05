7f5221d098242283874b0fbbf41bc185c39579fd
alumno <alumno>
2019-02-05 20:56:55 -0300

    5febrero

--- 1-EntradaSalida/jsEntradaSalida-4.js
+++ 1-EntradaSalida/jsEntradaSalida-4.js
@@ -4,6 +4,8 @@
 */
 function mostrar()
 {
-
+	var nombre;
+	nombre = prompt ("nombre");
+	document.getElementById("elNombre").value = nombre;
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,16 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+    var numero1;
+    var numero2;
+    var resultado;
+    numero1 = document.getElementById(numeroUno);
+    numero2 = document.getElementById(numeroDos);
+    parseInt(numero1)
+    parseInt(numero2)
+    resultado = parseInt(numero1) + parseInt(numero2);
+
+    alert("la suma es: " + resultado);
 
 }
 
