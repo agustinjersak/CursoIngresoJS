09428fa8163d2388e592d772f1400ddf59ac5c2d
alumno <alumno>
2019-02-11 22:00:07 -0300

    clase3

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,43 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+    var numero, numero1, numero2, suma;
+    numero = document.getElementById("PrecioUno").value;
+    numero1 = document.getElementById("PrecioDos").value;
+    numero2 = document.getElementById("PrecioTres").value;
+    parseInt(numero);
+    parseInt(numero1);
+    parseInt(numero2);
+    suma = parseInt(numero) + parseInt(numero1) + parseInt(numero2);
+    alert("La suma es: " + suma);
+
 	
 }
 function Promedio () 
 {
+    var numero,numero1,numero2,promedio;
+    numero = document.getElementById("PrecioUno").value;
+    numero1 = document.getElementById("PrecioDos").value;
+    numero2 = document.getElementById("PrecioTres").value;
+    parseInt(numero);
+    parseInt(numero1);
+    parseInt(numero2);
+    promedio = parseInt(numero) % parseInt(numero1) % parseInt(numero2);
+    alert("El promedio es: " + promedio)
+
 	
 }
 function PrecioFinal () 
 {
+    var numero, numero1, numero2, suma, resultado;
+    numero = document.getElementById("PrecioUno").value;
+    numero1 = document.getElementById("PrecioDos").value;
+    numero2 = document.getElementById("PrecioTres").value;
+    parseInt(numero);
+    parseInt(numero1);
+    parseInt(numero2);
+    suma = parseInt(numero) + parseInt(numero1) + parseInt(numero2);
+    resultado = suma + 21% 
+
 	
 }
