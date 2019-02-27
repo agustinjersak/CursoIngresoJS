91a01a94c784e8c2cb07f13e74b13e1b32c0b75c
agustinjersak <gugyjersak@gmail.com>
2019-02-26 22:06:03 -0300

    ultimaclase

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,48 @@
 function mostrar()
 {
+  var precio;
+  var paquete;
+  var pago;
+  var total;
+  var descuento;
+  precio = prompt("Ingresar el precio de su habitacion: ");
+  precio = parseInt(precio);
+  pago = prompt("Puede pagar con: Visa - PayPal - MercadoPago - Efectivo - Otro ");
+  paquete = prompt("Ingrese el paquete elegido: todoIncluido - soloDesayunos ");
+switch(pago){
+  case "Visa", "MercadoPago":
+  descuento = precio * 0.90;
+  break;
+  case "PayPal":
+  switch(paquete){
+    case "todoIncluido":
+    descuento = precio * 0.75;
+    break;
+
+    default:
+    descuento = precio * 0.85;
+    break;
+   }
+    case "Efectivo":
+    switch(paquete){
+      case "todoIncluido":
+      descuento = precio * 0.65;
+      break;
+      case "soloDesayunos":
+      descuento = precio * 0.70;
+      break;
+      default:
+      descuento = precio * 0.80;
+      break;
+    }
+      default:
+      descuento = precio * 0.95;
+      break;
+    }
+  total = precio * descuento;
+  alert("El precio final de la habitacion es: " + total);
+
 
 }
+
+
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,47 @@
 function mostrar()
 {
+    var importe1;
+    var importe2;
+    var importe3;
+    var importe4;
+    var suma;
+    importe1 = prompt("Ingrese un importe");
+    importe2 = prompt("Ingrese un importe");
+    importe3 = prompt("Ingrese un importe");
+    importe4 = prompt("Ingrese un importe");
+    suma = parseInt(importe1) + parseInt(importe2) + parseInt(importe3) + parseInt (importe4);
+    alert("Su importe es: " + suma);
+    if((importe1>importe2) && (importe1>importe3) && (importe1>importe4)){
+      alert("El importe mayor es: " + importe1);
+
+    }
+    else{
+      if((importe2>importe1) && (importe2>importe3) && (importe2>importe4))
+      alert("El importe mayor es: " + importe2);
+
+    }
+      if((importe3>importe1) && (importe3>importe2) && (importe3>importe4)){
+      alert("El importe mayor es: " + importe3);
+    }
+    else{
+      if((importe4>importe1) && (importe4>importe2) && (importe4>importe3))
+      alert("El importe mayor es: " + importe4);
+    }
+    if(suma > 100){
+      alert("Su importe con un 10% es: " + suma * 0.90);
+    }
+    else{
+      if((suma > 50) && (suma < 100))
+      alert("Su importe con un 5% es: " + suma * 0.95);
+    }
+    if(suma < 50){
+      alert("Su importe tiene un 15% de recargo: " + suma * 1.75);
+    }
+
+
+
+
+
+
 
 }
